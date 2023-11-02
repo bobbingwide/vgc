1a81fa1ddf7044ebcb344251dd30770d1fcb9f2b
Herb Miller <herb@bobbingwide.com>
2023-11-02 15:14:44 +0000

    Support PHP 8.1 #62

--- template-parts/shop/postcode-search.php
+++ template-parts/shop/postcode-search.php
@@ -4,7 +4,7 @@
              <div class="col text-center bg-brand-secondary">
 			<?php if(is_active_sidebar('postcode-search-text')) : dynamic_sidebar('postcode-search-text'); endif; ?>
 			<form action="#options_area" method="post">
-				<input type="text" name="postcode" id="postcode" placeholder="Enter here..." maxlength="4" value="<?php echo isset($_POST['postcode']) ? filter_var($_POST['postcode'], FILTER_SANITIZE_STRING) : ""; ?>">
+				<input type="text" name="postcode" id="postcode" placeholder="Enter here..." maxlength="4" value="<?php echo isset($_POST['postcode']) ? htmlspecialchars($_POST['postcode'] ) : ""; ?>">
 				<input type="submit" value="VIEW OPTIONS" class="submit_pc">
 			</form>
              </div>
