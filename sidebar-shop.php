c0c7fccf24e787cbcfb261e12a6070c7d6de541d
Herb Miller <herb@bobbingwide.com>
2025-07-10 20:19:40 +0100

    Add support for Search & Filter Pro v3 while still supporting v2 #71

--- sidebar-shop.php
+++ sidebar-shop.php
@@ -2,7 +2,19 @@
   <h2 class="fw-bold h5">Filter Search</h2>
 
 
-    <?php 
-        echo do_shortcode('[searchandfilter id="6288"]')
+    <?php
+    if ( !defined( 'SEARCH_FILTER_PRO_VERSION')) {
+        echo do_shortcode('[searchandfilter id="6288"]');
+    } else {
+        echo '<div class="searchandfilter">';
+        echo do_shortcode('[searchandfilter field="Submit Button"]');
+        echo do_shortcode('[searchandfilter field="Brand"]');
+        echo do_shortcode('[searchandfilter field="Product Building Type"]');
+        echo do_shortcode('[searchandfilter field="Material"]');
+        echo do_shortcode('[searchandfilter field="Size"]');
+        echo do_shortcode('[searchandfilter field="Reset"]');
+        echo do_shortcode('[searchandfilter field="Submit Button"]');
+        echo '</div>';
+    }
     ?>
 </sidebar>
