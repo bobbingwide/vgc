b310bb669ccaefa175443bd7b97c0c475b2c0e77
Herb Miller <herb@bobbingwide.com>
2025-10-14 14:41:54 +0100

    Performance: Use smaller icons in the USP slider #97

--- template-parts/shop/usp-strip.php
+++ template-parts/shop/usp-strip.php
@@ -5,27 +5,27 @@
     <div class="row d-flex bg-grey" id="uspstrip">
         <ul class="slides">
         <li class="col-lg-2 col-md-4 col-sm-4 text-center p-3">
-            <img src="/wp-content/themes/vgc/images/icons/lorry_500x500.png" alt="Free Delivery Icon"><br>
+            <img src="/wp-content/themes/vgc/images/icons/lorry_80x80.png" width="80" height="80" alt="Free Delivery Icon"><br>
             <strong class="w-100 clearfix">Free Delivery</strong>On Most Products
         </li>
         <li class="col-lg-2 col-md-4 col-sm-4 text-center p-3">
-            <img src="/wp-content/themes/vgc/images/icons/phone_500x500.png" alt="Call Centre Icon"><br>
+            <img src="/wp-content/themes/vgc/images/icons/phone_80x80.png" width="80" height="80" alt="Call Centre Icon"><br>
             <strong class="w-100 clearfix">Call Centre</strong>Based In The UK
         </li>
         <li class="col-lg-2 col-md-4 col-sm-4 text-center p-3">
-            <img src="/wp-content/themes/vgc/images/icons/shed_500x500.png" alt="Show Site Icon"><br>
+            <img src="/wp-content/themes/vgc/images/icons/shed_80x80.png" width="80" height="80" alt="Show Site Icon"><br>
             <strong class="w-100 clearfix">Show Site</strong>With Over 50 Buildings
         </li>
         <li class="col-lg-2 col-md-4 col-sm-4 text-center p-3">
-            <img src="/wp-content/themes/vgc/images/icons/bucket-and-wall_500x500.png" alt="Base &amp; Installation Icon"><br>
+            <img src="/wp-content/themes/vgc/images/icons/bucket-and-wall_80x80.png" width="80" height="80" alt="Base &amp; Installation Icon"><br>
             <strong class="w-100 clearfix">Base &amp; Installation</strong>Services Available
         </li>
         <li class="col-lg-2 col-md-4 col-sm-4 text-center p-3">
-            <img src="/wp-content/themes/vgc/images/icons/wreath_500x500.png" alt="Est. 1984 Icon"><br>
+            <img src="/wp-content/themes/vgc/images/icons/wreath_80x80.png" width="80" height="80" alt="Est. 1984 Icon"><br>
             <strong class="w-100 clearfix">Est. 1984</strong>Award Winning Company
         </li>
         <li class="col-lg-2 col-md-4 col-sm-4 text-center p-3">
-            <img src="/wp-content/themes/vgc/images/icons/shovel-in-dirt_500x500.png" alt="Landscaping &amp; Groundworks Icon"><br>
+            <img src="/wp-content/themes/vgc/images/icons/shovel-in-dirt_80x80.png" width="80" height="80" alt="Landscaping &amp; Groundworks Icon"><br>
             <strong class="w-100 clearfix">Landscaping &amp; Groundworks</strong>Services Available
         </li>
         </ul>
